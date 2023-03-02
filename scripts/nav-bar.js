ffe3aaa55b66f33ca563528e89ed048955d9aaa0
Sskyfu <skylar.shapiro.frost@gmail.com>
2023-03-02 11:09:15 -0800

    Added nav-bar, moved to top of page. Added personal statment.

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -1,11 +1,25 @@
 let navBar = document.getElementById("nav-bar");
 
+let pageList = ["Home", "Work", "Projects", "About", "Contact"]
+
 
 //test
-
+/*
 let btnTest = document.createElement("button");
 btnTest.classList.add("btn");
 btnTest.classList.add("btn-primary");
 btnTest.innerHTML = "Test Button";
 
 navBar.appendChild(btnTest);
+
+*/
+
+for (i =0; i < pageList.length; i++)
+{
+let btnTest = document.createElement("button");
+btnTest.classList.add("btn");
+btnTest.classList.add("btn-primary");
+btnTest.innerHTML = pageList[i];
+
+navBar.appendChild(btnTest);
+}
