4f6cedad78e910c4409c9b81f2734b63bee4f4fd
Sskyfu <skylar.shapiro.frost@gmail.com>
2023-03-02 11:36:33 -0800

    added other pages to site and changed buttons to links and set them to link to pages

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -16,10 +16,17 @@ navBar.appendChild(btnTest);
 
 for (i =0; i < pageList.length; i++)
 {
-let btnTest = document.createElement("button");
-btnTest.classList.add("btn");
-btnTest.classList.add("btn-primary");
-btnTest.innerHTML = pageList[i];
+let btnTemp = document.createElement("a");
+btnTemp.classList.add("btn");
+btnTemp.classList.add("btn-primary");
+btnTemp.innerHTML = pageList[i];
+btnTemp.setAttribute('href', './' + pageList[i] + '.html')
 
-navBar.appendChild(btnTest);
+//home workaround
+//TODO - try to find a more elegant way to handle this 
+
+if(pageList[i] == "Home"){btnTemp.setAttribute('href', './'+  "index" + '.html')}
+
+
+navBar.appendChild(btnTemp);
 }
